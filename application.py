bdee7c1f6574f0e043fde96880514848029532b5
Igor Dzhepko <DZH777@list.ru>
2019-08-26 12:57:41 +0300

    On branch master  Your branch is up to date with 'origin/master'.
     Changes to be committed:
    	modified:   application.py

--- application.py
+++ application.py
@@ -7,7 +7,7 @@ import math
 app = Flask(__name__)
  
 app.config["SESSION_FILE_DIR"] = mkdtemp()
-app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_PERMANENT"] = True
 app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
  
